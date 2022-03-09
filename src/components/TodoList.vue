d8eed8a85ddb551ea8feceffaa0aecb5e18f0342
Eunhye <junejc0613@naver.com>
2022-03-09 21:02:02 +0900

    [FEAT] Erase selected Todo Item

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,15 @@
   <section>
     <ul>
       <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+        <i class="checkBtn fa fa-check" aria-hidden="true"></i>
         {{ todoItem }}
+        <span
+          class="removeBtn"
+          type="button"
+          @click="removeTodo(todoItem, index)"
+        >
+          <i class="fa fa-trash" aria-hidden="true"></i>
+        </span>
       </li>
     </ul>
   </section>
@@ -24,8 +32,32 @@ export default {
       }
     }
   },
+  methods: {
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    },
+  },
 };
 </script>
 
 <style>
+ul {
+  list-style-type: none;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50ps;
+  margin: 0.5 0;
+  padding: 0 0.9;
+}
+.checkBtn {
+  margin-right: auto;
+}
+.removeBtn {
+  margin-left: auto;
+}
 </style>
