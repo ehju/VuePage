f0ce851c7eccfc413f50fdd15a4afdc5745dde0f
Eunhye <junejc0613@naver.com>
2022-03-10 21:32:11 +0900

    [FIX] main.js use BootstrapVue3

--- src/main.js
+++ src/main.js
@@ -6,6 +6,6 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
-createApp(App).mount('#app')
-App.use(BootstrapVue3)
-App.mount('#app')
+const app =createApp(App)
+app.mount('#app')
+app.use(BootstrapVue3)
